3af7c110e875c914f029429407e09ccdbf2677b5
Shiuan-Chang <135259951+Shiuan-Chang@users.noreply.github.com>
2026-05-20 18:14:42 -0600

    Add: anagram-groups - submission-13

---
+++ Data Structures & Algorithms/anagram-groups/submission-13.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String,List<String>> res = new HashMap<>();
+        for (int i = 0; i < strs.length; i++)
+        {
+            int[] count = new int[26];
+
+            for(char s : strs[i].toCharArray())
+            {
+                count[s - 'a'] = count[s - 'a'] + 1;
+            }
+
+            String key = Arrays.toString(count);
+            res.putIfAbsent(key,new ArrayList<>());
+            res.get(key).add(strs[i]);
+        }
+        return new ArrayList<>(res.values());
+
+        
+    }
+}
