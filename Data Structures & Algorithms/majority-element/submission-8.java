a3f0a8105c67783d72c3dc1775d932661d7a4f5d
Shiuan-Chang <135259951+Shiuan-Chang@users.noreply.github.com>
2026-09-02 20:17:18 -0600

    Add: majority-element - submission-8

---
+++ Data Structures & Algorithms/majority-element/submission-8.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int majorityElement(int[] nums) 
+    {
+        int count = 0;
+        int number = 0;
+        
+        for (int i = 0; i < nums.length; i++)
+        {
+            if(count == 0)
+            {
+                number = nums[i];
+                
+            }           
+            if (number != nums[i])
+            {
+                count -= 1;
+            }
+            else{ count += 1; }         
+        }
+        return number;       
+    }
+}
