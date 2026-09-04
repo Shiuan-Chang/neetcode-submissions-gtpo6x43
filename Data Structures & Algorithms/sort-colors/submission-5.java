d895cf460c79b502715386651318990270cbd9cc
Shiuan-Chang <135259951+Shiuan-Chang@users.noreply.github.com>
2026-09-04 17:29:16 -0600

    Add: sort-colors - submission-5

---
+++ Data Structures & Algorithms/sort-colors/submission-5.java
@@ -0,0 +1,28 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int l = 0;
+        int i = 0;
+        int r = nums.length-1;
+
+        while (i <= r){
+            if (nums[i] == 0){
+                swap(nums, l, i);
+                l++;
+                i++;
+            }
+            else if (nums[i] == 2){
+                swap(nums, i, r);
+                r--;
+            }
+            else{
+                i++;
+            }
+        }     
+    }
+}
+
+private void swap(int[] nums, int i, int j){
+    int temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp;
+}
